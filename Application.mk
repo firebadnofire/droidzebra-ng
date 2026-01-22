46d460c127c3505b27f220a62603b4eaa779c21e
firebadnofire <71282731+firebadnofire@users.noreply.github.com>
2026-01-22 13:18:11 -0500

    Add Gradle-based Android Studio project

--- Application.mk
+++ Application.mk
@@ -1,3 +1,4 @@
 APP_PROJECT_PATH := $(call my-dir)/project
 APP_MODULES      := droidzebra
-APP_PLATFORM	 := android-4
+APP_PLATFORM     := android-24
+APP_ABI          := armeabi-v7a arm64-v8a x86 x86_64
