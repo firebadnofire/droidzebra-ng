aff8df179a77cf9ea7b07a67eab2d67f138cbe72
firebadnofire <71282731+firebadnofire@users.noreply.github.com>
2026-01-22 14:36:51 -0500

    Limit status panel scaling on large screens

--- project/src/com/shurik/droidzebra/StatusView.java
+++ project/src/com/shurik/droidzebra/StatusView.java
@@ -348,7 +348,19 @@ public class StatusView extends View {
 	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		int measuredWidth = MeasureSpec.getSize(widthMeasureSpec);
+		int measuredHeight = MeasureSpec.getSize(heightMeasureSpec);
+		int maxHeightPx = (int)TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP,
+				480,
+				getResources().getDisplayMetrics());
+		int maxHeight = (int)Math.min(measuredWidth * 0.8f, maxHeightPx);
+		if(measuredHeight > 0) {
+			measuredHeight = Math.min(measuredHeight, maxHeight);
+		} else {
+			measuredHeight = Math.max(getSuggestedMinimumHeight(), maxHeight);
+		}
+		setMeasuredDimension(measuredWidth, measuredHeight);
 	}
 
 	@Override
